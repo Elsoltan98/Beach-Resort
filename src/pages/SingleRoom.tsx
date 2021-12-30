ca07323767ddbba2a9966e99f2ccbe68f1e36a78
MACBOOK <ben10a2992@gmail.com>
2021-12-30 14:37:21 +0200

    Start codeing in beach resort application

---
+++ src/pages/SingleRoom.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleRoom = () => {
+    return (
+        <div>
+            <p>Hello from SingleRoom page</p>
+        </div>
+    )
+}
+
+export default SingleRoom
