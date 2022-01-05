e8c2e3c9aaa3a2cfe3fa64209731cd5137893a29
MACBOOK <ben10a2992@gmail.com>
2022-01-05 12:37:13 +0200

    Add Services , title

---
+++ src/components/Services.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Title from './Title'
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa'
+
+const Services = () => {
+    const icons = [
+        {
+            id: 1,
+            icon: <FaCocktail />,
+            title: "Free cocktails",
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores.",
+        },
+        {
+            id: 2,
+            icon: <FaHiking />,
+            title: "Endless Hiking",
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores.",
+        },
+        {
+            id: 1,
+            icon: <FaShuttleVan />,
+            title: "Free Shuttle",
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores.",
+        },
+        {
+            id: 1,
+            icon: <FaBeer />,
+            title: "Strongest Beer",
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores.",
+        },
+    ]
+    return (
+        <section className="services">
+            <Title title="Services" />
+            <div className="services-center">
+                {icons.map(item => (
+                    <article key={item.id} className="services">
+                        <span>{item.icon}</span>
+                        <h6>{item.title}</h6>
+                        <p>{item.info}</p>
+                    </article>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Services
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Banner from '../components/Banner'
 import Hero from '../components/Hero'
+import Services from '../components/Services'
+
 
 const Home = () => {
+
     return (
+        <>
         <Hero hero="defaultHero">
             <Banner title="Luxurios rooms" subTitle="deluxe rooms starting at 299$">
                 <Link to="/rooms" className="btn-primary">
@@ -12,6 +16,8 @@ const Home = () => {
                 </Link>
             </Banner>
         </Hero>
+        <Services />
+        </>
     )
 }
 
