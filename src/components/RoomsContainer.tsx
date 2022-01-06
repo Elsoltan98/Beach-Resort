ccd9e8104952d893d5a3c08fee6d8a0c373fa9ec
MACBOOK <ben10a2992@gmail.com>
2022-01-06 22:39:17 +0200

    Create rooms container

---
+++ src/components/RoomsContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RoomsContainer = () => {
+    return (
+        <div>
+            Hello from rooms container
+        </div>
+    )
+}
+
+export default RoomsContainer
---
+++ src/components/RoomsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RoomsList = () => {
+    return (
+        <div>
+            Hello from rooms list
+        </div>
+    )
+}
+
+export default RoomsList
